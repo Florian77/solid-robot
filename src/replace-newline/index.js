a2863e2ad318973ba5e9eedf7d964fea0af2384b
Florian <info@ftws.de>
2020-09-28 17:14:50 +0200

    add emptyString

--- src/replace-newline/index.js
+++ src/replace-newline/index.js
@@ -2,12 +2,13 @@ const {replace, curryN} = require('ramda');
 
 const replaceNewline = curryN(2, (replaceWith, text) => replace( /(?:\r\n|\r|\n)/g, replaceWith, String(text)) );
 const withSpace = replaceNewline(' ');
-const withEmpty = replaceNewline('');
+const withEmptyString = replaceNewline('');
 const withBR = replaceNewline('<br>');
 
 module.exports.replace = replaceNewline;
 module.exports.space = withSpace;
-module.exports.empty = withEmpty;
+module.exports.empty = withEmptyString;
+module.exports.emptyString = withEmptyString;
 module.exports.br = withBR;
 
 
